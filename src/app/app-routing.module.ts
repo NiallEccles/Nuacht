dea5f2fd296e8d1ec738095f72a546c9774f783c
Niall <nialle@rothco.ie>
2020-02-19 14:17:13 +0000

    Added subroutes for categories.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ReaderComponent } from "./components/reader/reader.component";
+import { StarredComponent } from "./components/starred/starred.component";
+import { CategoryComponent } from "./components/category/category.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ReaderComponent },
+  { path: "starred", component: StarredComponent },
+  {
+    path: "category",
+    component: CategoryComponent,
+    children: [
+      { path: "business", component: CategoryComponent },
+      { path: "entertainment", component: CategoryComponent },
+      { path: "health", component: CategoryComponent },
+      { path: "science", component: CategoryComponent },
+      { path: "sports", component: CategoryComponent },
+      { path: "technology", component: CategoryComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
